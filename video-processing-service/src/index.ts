24eaf371071e6230ae2c50d50f97742bcae5652e
Tyler Jennings <tylerjennings2@outlook.com>
2024-05-29 15:57:48 -0400

    FEATURE-UPLOAD: Implemented backend endpoint for uploading video to convert locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,29 @@
 import express from 'express'
-
+import ffmpeg from 'fluent-ffmpeg'
 const app = express()
-const port = 3007
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/process-video', (req, res) => {
+  // Get path to video file from request
+  const inputFilePath = req.body.inputFilePath
+  const outputFilePath = req.body.outputFilePath
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send('Missing input or output file path')
+  }
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', () => {
+      res.status(200).send('Video Processing finished.')
+    })
+    .on('error', (err) => {
+      console.log('Error:', err)
+      res.status(500).send(`Internal server error: ${err.message}`)
+    })
+    .save(outputFilePath)
 })
 
+const port = process.env.PORT || 3007
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
