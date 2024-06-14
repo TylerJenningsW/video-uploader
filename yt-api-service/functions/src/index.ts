5afa32f5b0e19a01ce8b4cb7e7d17fc63f6e25ed
Tyler Jennings <tylerjennings2@outlook.com>
2024-06-14 10:20:53 -0400

    Setup get videos endpoint on cloudrun

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -12,6 +12,23 @@ const storage = new Storage();
 
 const rawVideoBucketName = "tj-yt-raw-videos";
 
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const querySnapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return querySnapshot.docs.map((doc) => doc.data());
+});
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
